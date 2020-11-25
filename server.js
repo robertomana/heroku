5ca22aefcec8f7c8a887f6f4cc2d4bf74daee0f2
robertomana <ing.mana@tiscali.it>
2020-11-25 16:56:11 +0100

    database testing

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ let mongo = require("mongodb");
 let mongoClient = mongo.MongoClient;
 const ObjectId = mongo.ObjectId;
 //const CONNECTIONSTRING = "mongodb://127.0.0.1:27017";
-const CONNECTIONSTRING = "mongodb+srv://robertomana:pass-word@cluster0.zzmgh.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
+const CONNECTIONSTRING = "mongodb+srv://robertomana:vallauri123@cluster0.zzmgh.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority"
 const CONNECTIONOPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
 
 
